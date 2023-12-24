4b8880ad907e8f7185fb3b7a48fb306b97721349
antlers <antlers@illucid.net>
2023-12-24 13:01:13 +0000

    WIP

--- dashboard.py
+++ dashboard.py
@@ -5,17 +5,16 @@ import streamlit.components.v1 as components
 import datetime
 import random
 import io
+from functools import partial
 
 @st.cache_data
 def default_date():
     return datetime.datetime.utcnow() - datetime.timedelta(days=1)
 
 @st.cache_data
-def get_toots(_cache_key: int, timeline_since, n_clusters) -> list[core.Toot]:
+def get_toots(_cache_key: int, timeline_since) -> list[core.Toot]:
     print("get_toots", _cache_key, st.session_state.cache_key)
     toots = core.Toot.get_toots_since(datetime.datetime.utcnow() - timeline_since)
-    if len(toots) > 0:
-        science.assign_clusters(toots, n_clusters=n_clusters)
     return toots
 
 # Refresh button
@@ -36,29 +35,89 @@ else:
             core.download_timeline(latest_date)
         st.session_state.cache_key = random.randint(0, 10000)
 
+if st.button("Reset"):
+    st.session_state.cache_key = random.randint(0, 10000)
+
 # customize timeline segment to analyze
 timeline_since = ui.get_time_frame()
 
 # customize clustering algo
 n_clusters = st.slider("Number of clusters", 2, 20, 5)
 
+kmeans_algo = st.radio("KMeans Algo", ["Spectral", "Standard"])
+
 if "cache_key" not in st.session_state:
     print("init cache_key", st.session_state)
     st.session_state.cache_key = random.randint(0, 10000)
 
-if st.button("Show"):
-    st.session_state.cache_key = random.randint(0, 10000)
+# Just keeping a list~
+if "cache_tracker" not in st.session_state:
+    print("init cache tracker", st.session_state)
+    st.session_state.cache_tracker = []
+def current_hash():
+    hash((st.session_state.cache_key, kmeans_algo, n_clusters, timeline_since))
+def current_hash_cached():
+    if current_hash() in st.session_state.cache_tracker:
+       return True
+    else:
+       return False
+def current_hash_cache():
+    if not current_hash_cached():
+        st.session_state.cache_tracker += [current_hash()]
 
 print(f"state: {st.session_state.cache_key}")
 
-toots = get_toots(st.session_state.cache_key, timeline_since, n_clusters)
-clusters = sorted(list({t.cluster for t in toots if t.cluster}))
+toots = get_toots(st.session_state.cache_key, timeline_since)
+toots = [toot for toot in toots if toot.embedding is not None]
+
+# This is just a hack for labels, actual desc. stuff is still at EOF.
+if len(toots) != 0:
+    embeddings, labels = science.assign_clusters(st.session_state.cache_key, toots, kmeans_algo, n_clusters=n_clusters)
+    toot_desc = {}
+    if current_hash_cached():
+        for cluster_toots, count, desc in science.describe_clusters(hash, toots, labels):
+            for toot in cluster_toots:
+                toot_desc[toot] = desc
+
+def format_node(i, toot, x, y):
+    c = toot.content
+    content = c[:97] + '...' if len(c) > 100 else c
+
+    if current_hash_cached() and toot_desc[toot] != "":
+        label = toot_desc[toot]
+        if len(label) > 45:
+            label = label[:42] + "..."
+    else:
+        label = i
+
+    return {
+        "Cluster": label,
+        "x": x,
+        "y": y,
+        "author": toot.author,
+        "content": content,
+        # This doesn't seem to work very consistently, and I wanted to be able
+        # to float-left the graph and hover over toots to highlight everything
+        # from that cluster or account (so, interactive the other way around)
+        # but ah well that's what I get for not attaching JS triggers to an
+        # unencoded matplotlib SVG.
+        "url": "#" + str(toot.id)
+    }
+
 if len(toots) == 0:
     st.markdown("No toots found. Try clicking **Download toots** or **Refresh toots** above and then click **Show**.")
 else:
+    data=[]
+    for i in range(n_clusters):
+      data.extend(map(partial(format_node, i),
+                      toots,
+                      embeddings[labels == i, 0],
+                      embeddings[labels == i, 1],))
     st.vega_lite_chart(
-        science.format_for_vega(toots, n_clusters),
+        data,
         {
+            "width": 700,
+            "height": 500,
             "mark": {
                 "type": "circle",
                 "size": 60,
@@ -69,10 +128,11 @@ else:
                 "select": "interval",
                 "bind": "scales"
             }],
+            "config": {"axis": {"title": False}},
             "encoding": {
-                "x": {"field": "x", "type": "quantitative"},
-                "y": {"field": "y", "type": "quantitative"},
-                "color": {"field": "label", "type": "nominal"},
+                "x": {"field": "x", "type": "quantitative", "scale": {"domain": [-9, 11]}},
+                "y": {"field": "y", "type": "quantitative", "scale": {"domain": [-7, 13]}},
+                "color": {"field": "Cluster", "type": "nominal"},
                 "href": {"field": "url", "type": "nominal"},
                 "tooltip": [
                     {"field": "author", "type": "nominal"},
@@ -80,10 +140,15 @@ else:
                 ]
             }
         })
-    for cluster in clusters:
-        cluster_count = len([t for t in toots if t.cluster == cluster])
-        with st.expander(f"{cluster} ({cluster_count} toots)"):
-            for toot in toots:
-                if toot.cluster == cluster:
+    is_labeling = st.button("Label clusters", disabled=current_hash_cached())
+    if is_labeling:
+        with st.spinner("Labeling clusters..."):
+            science.describe_toots(toots)
+            current_hash_cache()
+    if current_hash_cached():
+        for toots, count, desc in science.describe_clusters(hash, toots, labels):
+            with st.expander(f"{desc} ({count} toots)"):
+                for toot in toots:
                     ui.display_toot(toot)
+
     # import code; code.interact(local=locals())
--- fossil/science.py
+++ fossil/science.py
@@ -5,123 +5,63 @@ import streamlit as st
 from sklearn.cluster import KMeans, SpectralClustering
 import openai
 from time import sleep
-from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# def print_graphs(embeddings):
-#   for n_clusters in range(2, 8):
-#     kmeans = KMeans(n_clusters=n_clusters)
-#     labels = kmeans.fit_predict(embeddings)
-#     u_labels = np.unique(labels)
-#     for i in u_labels:
-#       plt.scatter(embeddings[label == i, 0], embeddings[label == i, 1], label = i)
-#     plt.legend()
-#     plt.show()
+def resolve_kmeans_algo(kmeans_algo, n_clusters):
+  if kmeans_algo == "Standard":
+      return KMeans(n_clusters=n_clusters)
+  elif kmeans_algo == "Spectral":
+      return SpectralClustering(
+                 n_clusters=n_clusters,
+                 affinity='nearest_neighbors',
+                 assign_labels='kmeans')
 
-def get_it(prompt, client):
-  response = None
-  while response == None:
-    try:
-      response = client.chat.completions.create(
-                   model=config.SUMMARIZE_MODEL.name,
-                   messages=[{"role": "user", "content": core.reduce_size(prompt)}],
-                   max_tokens=100)
-      if response is not None:
-        return response
-      else:
-        raise ValueError('response is None')
-    except openai.InternalServerError:
-      continue
-    except ValueError:
-      continue
-
-# def print_graphs(embeddings):
-#   for n_clusters in range(2, 8):
-#     kmeans = KMeans(n_clusters=n_clusters)
-#     labels = kmeans.fit_predict(embeddings)
-#     u_labels = np.unique(labels)
-#     for i in u_labels:
-#       plt.scatter(embeddings[label == i, 0], embeddings[label == i, 1], label = i)
-#     plt.legend()
-#     plt.show()
-
-def get_it(prompt, client):
-  response = None
-  while response == None:
-    try:
-      response = client.chat.completions.create(
-                   model=config.SUMMARIZE_MODEL.name,
-                   messages=[{"role": "user", "content": core.reduce_size(prompt)}],
-                   max_tokens=100)
-      if response is not None:
-        return response
-      else:
-        raise ValueError('response is None')
-    except openai.InternalServerError:
-      continue
-    except ValueError:
-      continue
-
-def format_node(i, toot, x, y):
-    c = toot.content
-    content = c[:97] + '...' if len(c) > 100 else c
-    return {
-        "label": i,
-        "x": x,
-        "y": y,
-        "author": toot.author,
-        "content": content,
-        "url": "#" + str(toot.id)
-    }
-
-def format_for_vega(toots: list[core.Toot], n_clusters: int = 5):
-    embeddings = np.array([toot.embedding for toot in toots if type(toot.embedding) != type(None)])
-
-    # kmeans = SpectralClustering(
-    #            n_clusters=5,
-    #            affinity='nearest_neighbors',
-    #            assign_labels='kmeans')
-    kmeans = KMeans(n_clusters=n_clusters)
-    cluster_labels = kmeans.fit_predict(embeddings)
-
-    # XXX: Someone who knows python could probably write this more clearly?
-    data = []
-    for i in range(0, n_clusters):
-      data.extend(map(partial(format_node, i),
-                      toots,
-                      embeddings[cluster_labels == i, 0],
-                      embeddings[cluster_labels == i, 1],))
-    return data
-
-def assign_clusters(toots: list[core.Toot], n_clusters: int = 5):
+@st.cache_data
+def assign_clusters(hash: int, _toots: list[core.Toot], kmeans_algo, n_clusters):
     print('Assigning')
-    # meh, ignore toots without content. I think this might be just an image, not sure
-    toots = [toot for toot in toots if toot.embedding is not None]
+    kmeans = resolve_kmeans_algo(kmeans_algo, n_clusters)
+    embeddings = np.array([toot.embedding for toot in _toots if type(toot.embedding) != type(None)])
+    labels = kmeans.fit_predict(embeddings)
+    return embeddings, labels
 
-    # Perform k-means clustering on the embeddings
-    embeddings = np.array([toot.embedding for toot in toots if type(toot.embedding) != type(None)])
+def generate_with_retries(prompt, client):
+  response = None
+  while response == None:
+    try:
+      response = client.chat.completions.create(
+                   model=config.SUMMARIZE_MODEL.name,
+                   messages=[{"role": "user", "content": core.reduce_size(prompt)}],
+                   max_tokens=100)
+      if response is not None:
+        return response
+      else:
+        raise ValueError('response is None')
+    except openai.InternalServerError:
+      sleep(10)
+      continue
+    except ValueError:
+      sleep(10)
+      continue
 
-    # kmeans = SpectralClustering(
-    #            n_clusters=5,
-    #            affinity='nearest_neighbors',
-    #            assign_labels='kmeans')
-    kmeans = KMeans(n_clusters=n_clusters)
-    cluster_labels = kmeans.fit_predict(embeddings)
-
+def describe_toots(toots: list[core.Toot]):
     client = openai.OpenAI(api_key=config.OPENAI_KEY, base_url="http://localhost:8080/v1")
-    for i_clusters in range(n_clusters):
-        clustered_toots = [toot for toot, cluster_label in zip(toots, cluster_labels) if cluster_label == i_clusters]
-        combined_text = "\n\n".join([toot.content for toot in clustered_toots])
+    joined_text = "\n\n".join([toot.content for toot in toots])
 
-        # Use GPT-3.5-turbo to summarize the combined text
-        prompt = f"Create a single label that describes all of these related tweets, make it succinct but descriptive. The label should describe all {len(clustered_toots)} of these\n\n{combined_text}"
-        response = get_it(prompt, client)
-        summary = response.choices[0].message.content.strip()
+    # Use GPT-3.5-turbo to summarize the joined text
+    prompt = f"Create a single label that describes all of these related tweets, make it succinct but descriptive. The label should describe all {len(toots)} of these:\n\n{joined_text}\nSummery label: "
+    response = generate_with_retries(prompt, client)
+    summary = response.choices[0].message.content.strip()
 
-        # Do something with the summary
-        for toot, cluster_label in zip(toots, cluster_labels):
-            if cluster_label == i_clusters:
-                toot.cluster = summary
+    return summary
 
+@st.cache_data
+def describe_clusters(hash: int, _toots: list[core.Toot], labels: list[int]):
+    clusters = {label: {"toots": []} for label in labels}
+    for toot, label in zip(_toots, labels):
+        clusters[label]['toots'] += [toot]
+    for label, cluster in clusters.items():
+        cluster['count'] = len(cluster['toots'])
+        cluster['desc'] = describe_toots(cluster['toots'])
+    return [(c['toots'], c['count'], c['desc']) for c in clusters.values()]
