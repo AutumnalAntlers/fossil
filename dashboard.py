6edbe3514f5b3d8599945275520c053ba6ec179f
antlers <antlers@illucid.net>
2023-12-25 05:15:47 +0000

    WIP

--- dashboard.py
+++ dashboard.py
@@ -50,41 +50,79 @@ if "cache_key" not in st.session_state:
     print("init cache_key", st.session_state)
     st.session_state.cache_key = random.randint(0, 10000)
 
-# Just keeping a list~
-if "cache_tracker" not in st.session_state:
-    print("init cache tracker", st.session_state)
-    st.session_state.cache_tracker = []
-def current_hash():
-    hash((st.session_state.cache_key, kmeans_algo, n_clusters, timeline_since))
-def current_hash_cached():
-    if current_hash() in st.session_state.cache_tracker:
-       return True
-    else:
-       return False
-def current_hash_cache():
-    if not current_hash_cached():
-        st.session_state.cache_tracker += [current_hash()]
+from streamlit.runtime.caching.cache_utils import _make_value_key
+from streamlit.runtime.caching.cache_errors import CacheKeyNotFoundError
+import types
+from typing import Any
+
+def is_cached(
+        func: types.FunctionType,
+        args: tuple[Any, ...],
+        kwargs: dict[str, Any] = {}
+    ) -> tuple[bool, Any]:
+
+    cached_func = func.__dict__['clear'].__self__
+    info = cached_func._info
+    cache = info.get_function_cache(cached_func._function_key)
+    value_key= _make_value_key(
+        cache_type=info.cache_type,
+        func=info.func,
+        func_args=args,
+        func_kwargs=kwargs,
+        hash_funcs=info.hash_funcs
+    )
+
+    try:
+        return True, cache.read_result(value_key).value
+    except CacheKeyNotFoundError:
+        return False, None
+
+@st.cache_data
+def test(var):
+    return var
+
+cached_p, value = is_cached(test, (1,))
+st.write((cached_p, value))
+# => (False, None)
+
+test(1)
+cached_p, calue = is_cached(test, (1,))
+st.write((cached_p, value))
+# => (True, 1)
 
 print(f"state: {st.session_state.cache_key}")
 
 toots = get_toots(st.session_state.cache_key, timeline_since)
 toots = [toot for toot in toots if toot.embedding is not None]
 
-# This is just a hack for labels, actual desc. stuff is still at EOF.
-if len(toots) != 0:
+def current_hash():
+    hash((st.session_state.cache_key, kmeans_algo, n_clusters, timeline_since))
+
+if "last_clusters" not in st.session_state:
+    st.session_state.last_clusters = None
+
+# Initialize `described_clusters' to cached value or empty list
+if not len(toots) == 0:
     embeddings, labels = science.assign_clusters(st.session_state.cache_key, toots, kmeans_algo, n_clusters=n_clusters)
-    toot_desc = {}
-    if current_hash_cached():
-        for cluster_toots, count, desc in science.describe_clusters(hash, toots, labels):
-            for toot in cluster_toots:
-                toot_desc[toot] = desc
+    cached, described_clusters = is_cached(science.describe_clusters, (current_hash(), toots, labels), {})
+    if described_clusters is not None:
+        st.session_state.last_clusters = (current_hash(), described_clusters)
+    elif st.session_state.last_clusters is not None:
+        described_clusters = st.session_state.last_clusters[1]
+    else:
+        described_clusters = []
 
 def format_node(i, toot, x, y):
     c = toot.content
     content = c[:97] + '...' if len(c) > 100 else c
 
-    if current_hash_cached() and toot_desc[toot] != "":
-        label = toot_desc[toot]
+    descs = [
+        desc
+        for toots, count, desc in described_clusters
+        if toot in toots
+    ]
+    if cached and len(descs) > 0:
+        label = descs[0]
         if len(label) > 45:
             label = label[:42] + "..."
     else:
@@ -140,15 +178,15 @@ else:
                 ]
             }
         })
-    is_labeling = st.button("Label clusters", disabled=current_hash_cached())
-    if is_labeling:
-        with st.spinner("Labeling clusters..."):
-            science.describe_toots(toots)
-            current_hash_cache()
-    if current_hash_cached():
-        for toots, count, desc in science.describe_clusters(hash, toots, labels):
-            with st.expander(f"{desc} ({count} toots)"):
-                for toot in toots:
-                    ui.display_toot(toot)
+    describe_button_type = 'primary' if st.session_state.last_clusters is not None and current_hash() != st.session_state.last_clusters[0] else 'secondary'
+    is_describing = st.button("Describe clusters", disabled=cached, type=describe_button_type)
+    if is_describing:
+        with st.spinner("Describing clusters..."):
+            science.describe_clusters(current_hash(), toots, labels)
+            st.session_state.last_clusters = (current_hash(), described_clusters)
+    for toots, count, desc in described_clusters:
+        with st.expander(f"{desc} ({count} toots)"):
+            for toot in toots:
+                ui.display_toot(toot)
 
     # import code; code.interact(local=locals())
